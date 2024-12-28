7edcc3094da3d4e177fe6fe6d0a0ea6ff1fba809
Subhajit Das <subhajitdasw@gmail.com>
2024-12-28 15:53:36 -0800

    This commit adds memory ordering specs.

--- include/queue.hpp
+++ include/queue.hpp
@@ -37,7 +37,7 @@ bool Queue<T>::enqueue(T value)
 
     while (true)
     {
-        Pointer<T> tail = this->tail.load();
+        Pointer<T> tail = this->tail.load(std::memory_order_acquire);
         // invariant: tail always points to a valid node
         Pointer<T> tailNext = tail.ptr->next;
 
@@ -48,14 +48,14 @@ bool Queue<T>::enqueue(T value)
             {
                 // newNode pointer is nullptr
                 Pointer<T> newPointer(newNode, tail.count + 1);
-                tail.ptr->next.compare_exchange_weak(tailNext, newPointer);
+                tail.ptr->next.compare_exchange_weak(tailNext, newPointer, std::memory_order_release, std::memory_order_relaxed);
                 break;
             }
             else
             {
                 // go to next pointer
                 Pointer<T> newTail(tailNext.ptr, tail.count + 1);
-                this->tail.compare_exchange_weak(tail, newTail);
+                this->tail.compare_exchange_weak(tail, newTail, std::memory_order_acquire, std::memory_order_relaxed);
             }
         }
     }
@@ -63,7 +63,7 @@ bool Queue<T>::enqueue(T value)
     // we're done, swing tail over
     Pointer<T> oldTail = this->tail.load();
     Pointer<T> newTail(newNode, oldTail.count + 1);
-    this->tail.compare_exchange_weak(oldTail, newTail);
+    this->tail.compare_exchange_weak(oldTail, newTail, std::memory_order_release, std::memory_order_relaxed);
 }
 
 template <typename T>
@@ -72,9 +72,9 @@ bool Queue<T>::dequeue()
 
     while (true)
     {
-        Pointer<T> head = this->head.load();
+        Pointer<T> head = this->head.load(std::memory_order_acquire);
         Pointer<T> headNext = head.ptr->next;
-        Pointer<T> tail = this->tail.load();
+        Pointer<T> tail = this->tail.load(std::memory_order_acquire);
 
         // head and tail are same. the queue is empty
         if (head == this->head.load())
@@ -91,13 +91,13 @@ bool Queue<T>::dequeue()
                     // tail is falling behind
                     // go to next pointer
                     Pointer<T> newTail(headNext.ptr, tail.count + 1);
-                    this->tail.compare_exchange_weak(tail, headNext);
+                    this->tail.compare_exchange_weak(tail, headNext, std::memory_order_acquire, std::memory_order_relaxed);
                 }
             }
             else
             {
                 Pointer<T> newHead(headNext.ptr, head.count + 1);
-                this->head.compare_exchange_weak(head, newHead);
+                this->head.compare_exchange_weak(head, newHead, std::memory_order_acquire, std::memory_order_relaxed);
                 break;
             }
         }
